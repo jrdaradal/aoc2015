421a9707faffd12492984160091760351aae223e
roidaradal <johnroy.daradal@goodkredit.com>
2025-04-07 11:20:54 +0800

    v0.4.2 - Day 04.2

--- day04.py
+++ day04.py
@@ -5,6 +5,7 @@ import hashlib
 from utils import * 
 
 # SolutionA: 254575
+# SolutionB: 1038736
 
 def input04(full: bool) -> list[str]:
     return readLines(getPath(4, full))
@@ -12,14 +13,24 @@ def input04(full: bool) -> list[str]:
 def day04A():
     full = True
     for key in input04(full):
-        i = 0 
-        while True:
-            i += 1
-            fkey = '%s%d' % (key, i)
-            hash = hashlib.md5(fkey.encode('utf-8')).hexdigest()
-            if hash[:5] == '00000':
-                print(i)
-                break
+        findMin(key, 5)
+
+def day04B():
+    full = True 
+    for key in input04(full):
+        findMin(key, 6)
+
+def findMin(key: str, numZeros: int):
+    goal = '0' * numZeros
+    i = 0 
+    while True:
+        i += 1
+        fkey = '%s%d' % (key, i)
+        hash = hashlib.md5(fkey.encode('utf-8')).hexdigest()
+        if hash[:numZeros] == goal:
+            print(i)
+            break
 
 if __name__ == '__main__':
-    day04A()
+    # day04A()
+    day04B()
