7b1133faf68852ba7908bc935ebf4ac6fbfd5961
roidaradal <johnroy.daradal@goodkredit.com>
2025-04-08 21:34:55 +0800

    v0.8.2 - Day 08.2

--- day08.py
+++ day08.py
@@ -5,6 +5,7 @@ import re
 from utils import * 
 
 # SolutionA: 1350
+# SolutionB: 2085
 
 def input08(full: bool) -> list[str]:
     return readLines(getPath(8, full))
@@ -17,6 +18,14 @@ def day08A():
         total += len(text) - chars 
     print('Total:', total)
 
+def day08B():
+    full = True 
+    total = 0 
+    for text in input08(full):
+        chars = len(expandString(text))
+        total += chars - len(text)
+    print('Total:', total)
+
 def parseString(text: str) -> str:
     text = text[1:-1]
     hex = r'\\x[0-9a-f]{2}'
@@ -25,5 +34,17 @@ def parseString(text: str) -> str:
     text = text.replace('\\\\', '.')
     return text
 
+def expandString(text: str) -> str:
+    chars = []
+    for x in text:
+        if x == '"':
+            chars.append('\\"')
+        elif x == '\\':
+            chars.append('\\\\')
+        else:
+            chars.append(x)
+    return '"%s"' % ''.join(chars)
+
 if __name__ == '__main__':
-    day08A()
+    # day08A()
+    day08B()
